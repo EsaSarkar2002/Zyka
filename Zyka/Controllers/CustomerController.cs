05f6b5a59d02342e602edec554bd8409df7c7edc
2461411 <2461411@LTIN673961>
2026-01-09 17:22:17 +0530

    created reservations, gallery and about us pages

---
+++ Zyka/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zyka.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+
+        public IActionResult Reservation()
+        {
+            return View();
+        }
+
+        public IActionResult Booking()
+        {
+            return View();
+        }
+
+
+    }
+
+
+}
